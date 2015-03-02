69e368b3c3bf7685bb342c62b14b4a9094749a36
Denis Laliberté <denis@tp1.ca>
2015-03-02 18:44:44 -0500

    build script

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
+echo "Export html"
+
 pandoc --template=resume/template/initializr.html resume/readme.md -o index.html
 
+open ./index.html
+
+echo "export pdf"
+pandoc resume/readme.md -o pdf/2015_DenisLaliberte_.pdf
+
+open pdf/2015_DenisLaliberte_.pdf
+
+
+echo "export reamde"
 cat resume/readme-header.md resume/readme.md > README.md
 
