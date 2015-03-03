3974e26320232de77bd17e20a41b0a9cf2b09b79
Denis Laliberté <denis@tp1.ca>
2015-03-03 17:18:06 -0500

    build scrip

--- build.sh
+++ build.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+echo "cleanup (remove cv folders content)"
+sudo rm -r private/cv
+rm cv/*
+mkdir private
+mkdir private/cv
 
 echo "export html"
 cat data/header.md data/content.md > cv/2015_Denis_Laliberte_.md
 pandoc --template=template/initializr.html cv/2015_Denis_Laliberte_.md -o index.html
 
 echo "export pdf"
-rm cv/2015_Denis_Laliberte_.pdf
 pandoc cv/2015_Denis_Laliberte_.md -o cv/2015_Denis_Laliberte_.pdf
 
 echo "export private pdf"
-cat private/header.md data/content.md > private/2015_Denis_Laliberte_.md
-rm private/2015_Denis_Laliberte_.pdf
-pandoc private/2015_Denis_Laliberte_.md -o private/2015_Denis_Laliberte_.pdf
+cat private/header.md data/content.md > private/cv/2015_Denis_Laliberte_.md
+pandoc private/cv/2015_Denis_Laliberte_.md -o private/cv/2015_Denis_Laliberte_.pdf
 
 echo "export reamde"
 cat data/readme-header.md data/content.md > README.md
