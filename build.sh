b47148019cddcc86d9ca8768a8ddd036fb7ab822
Denis Laliberté <denis@tp1.ca>
2015-03-03 13:19:45 -0500

    add bootstrap theme

--- build.sh
+++ build.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
 echo "Export html"
-
 pandoc --template=cv/template/initializr.html cv/2015_Denis_Laliberte_.md -o index.html
 
-open ./index.html
-
 echo "export pdf"
-
 rm cv/2015_Denis_Laliberte_.pdf
 pandoc cv/2015_Denis_Laliberte_.md -o cv/2015_Denis_Laliberte_.pdf
-
-open cv/2015_Denis_Laliberte_.pdf
-
 
 echo "export reamde"
 cat cv/readme-header.md cv/2015_Denis_Laliberte_.md > README.md
