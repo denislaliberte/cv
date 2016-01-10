3c5fb4dc4cf782d9a856f8ea25cd2cc63f59dd50
Denis Laliberté <denis@tp1.ca>
2016-01-10 11:20:25 -0500

    osedea change year 2016

--- build.sh
+++ build.sh
@@ -14,18 +14,18 @@ mkdir private
 mkdir private/cv
 
 echo "export html"
-cat data/header.md data/content.md > cv/2015_Denis_Laliberte_.md
-cat data/header.md data/en-content.md > en/2015_Denis_Laliberte_.md
+cat data/header.md data/content.md > cv/2016_Denis_Laliberte_.md
+cat data/header.md data/en-content.md > en/2016_Denis_Laliberte_.md
 pandoc --template=template/initializr.html data/content.md -o index.html
 pandoc --template=template/initializr.html data/en-content.md -o en/index.html
 
 echo "export pdf"
-pandoc cv/2015_Denis_Laliberte_.md -o cv/2015_Denis_Laliberte_.pdf
-pandoc en/2015_Denis_Laliberte_.md -o en/2015_Denis_Laliberte_.pdf
+pandoc cv/2016_Denis_Laliberte_.md -o cv/2016_Denis_Laliberte_.pdf
+pandoc en/2016_Denis_Laliberte_.md -o en/2016_Denis_Laliberte_.pdf
 
 echo "export private pdf"
-cat private/header.md data/content.md > private/cv/2015_Denis_Laliberte_.md
-pandoc private/cv/2015_Denis_Laliberte_.md -o private/cv/2015_Denis_Laliberte_.pdf
+cat private/header.md data/content.md > private/cv/2016_Denis_Laliberte_.md
+pandoc private/cv/2016_Denis_Laliberte_.md -o private/cv/2016_Denis_Laliberte_.pdf
 
 echo "export reamde"
 cat data/readme-header.md data/content.md > README.md
