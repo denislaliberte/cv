be3d698ca52a6c7181403ccfe99b38fde701a872
Denis Laliberté <denis@tp1.ca>
2016-04-24 10:59:13 -0400

    wip refactor build script and add index.md

--- build.sh
+++ build.sh
@@ -16,8 +16,10 @@ mkdir private/cv
 echo "export html"
 cat data/header.md data/content.md > cv/2016_Denis_Laliberte_.md
 cat data/header.md data/en-content.md > en/2016_Denis_Laliberte_.md
-pandoc --template=template/initializr.html data/content.md -o index.html
-pandoc --template=template/initializr.html data/en-content.md -o en/index.html
+pandoc --template=template/initializr.html data/content.md -o cv/index.html
+pandoc --template=template/initializr.html data/index.md -o index.html
+pandoc --template=template/initializr.html data/index.md -o en/index.html
+pandoc --template=template/initializr.html data/en-content.md -o en/cv/index.html
 
 echo "export pdf"
 pandoc cv/2016_Denis_Laliberte_.md -o cv/2016_Denis_Laliberte_.pdf
